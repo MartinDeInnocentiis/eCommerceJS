748e1089dd93efbdc3ef9ec4af379a962bafc009
Martin De Innocentiis <martindeinnocentiis90@gmail.com>
2023-04-17 18:57:00 -0300

    Se agregó una API para consumir y crear los productos a partir de ella, usando fetch para el manejo de esa promesa

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*
 -------------------------------------------
-           ENTREGA 03: ECOMMERCE
+           ENTREGA FINAL: ECOMMERCE
            Martin De Innocentiis           
 -------------------------------------------
 */
@@ -12,45 +12,74 @@ const inputNombre = document.getElementById("nombre")
 const inputApellido = document.getElementById("apellido")
 const h2 = document.getElementById("h2")
 const div = document.getElementById("div")
-const advertencia=document.getElementById("advertencia")
+const advertencia = document.getElementById("advertencia")
+let productosFiltrados = [];
 
-//CREO UNA CLASE "PRODUCT"
-class Product {
-    constructor(id, nombre, precio, stock) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-    }
+
+/*CREO UNA FUNCION QUE: 
+CONFIGURA EL ACCESO A LA API A CONSUMIR
+MANEJA LA PROMESA (RESULTADO DEL LLAMADO A LA API)
+CREA DISTINTOS DIVS(PRODUCTOS) CON CADA RESULTADO
+SE APLICAN DIVERSOS FILTROS PARA ELIMINAR RESULTADOS INDESEADOS
+*/
+
+function getJuegos() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'b426f84182mshf6e6dae0876a14cp1e2843jsnead4abc5cf2c',
+            'X-RapidAPI-Host': 'steam2.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://steam2.p.rapidapi.com/search/Counter/page/1', options)
+        .then(response => response.json())
+        .then(response => {
+            const productosAPI = response;
+            console.log(response) //LOS VALORES DE LA PROPIEDAD PRICE ARROJABAN DIVERSOS RESULTADOS INDESEADOS, LO CUAL REQUIRIÓ APLICAR DIVERSOS FILTROS HASTA OBTENER SIMPLEMENTE EL FLOAT
+            const productos = productosAPI.filter(juego => {
+
+                const precio = juego.price ? juego.price.match(/[\d,]+/g) : '';  //SE USA LA EXPRESION [\d,] Y length PARA DETERMINAR SI HAY MÁS DE UN VALOR (HABIA RESULTADOS DOBLES), DETERMINANDO SI HAY MAS DE UNA COMA
+                return precio && precio.length === 1 && precio[0] !== "Free to Play" && precio[0] !== "Free Demo"; //SE FILTRAN TAMBIEN LOS RESULTADOS VACIOS Y "FREE/TO PLAY/DEMO"
+            }).map(juego => {
+                return {
+                    ...juego,
+                    price: parseFloat(juego.price.replace(/[^0-9,]/g, '').replace(',', '.'))
+                }
+            });
+            // AGREGO LOS PRODUCTOS AL CARRITO DE PRODUCTOS
+            productosFiltrados = productos.filter(prod => prod.appId)
+
+            // CREO LA TARJETA DE CADA PRODUCTO, PARA TODOS
+            productosFiltrados.forEach(prod => {
+
+                div.innerHTML += `<div class="card" style="width: 18rem";>
+                                        <div class="card-body">
+                                        <h5 class="card-title">${prod.title}</h5>
+                                        <p class="card-text">US$ ${prod.price}</p>
+                                        <button id=${prod.appId} class="btn btn-warning card-btn" style="background-color:#DDDA56; color:585846;">AGREGAR</button>  
+                                        </div>
+                                    </div>`;
+
+
+            });
+
+            // CAPTURAMOS TODOS LOS BOTONES 'Agregar' DE CADA PRODUCTO, Y LE AGREGAMOS EL EventListener 'agregarProducto'
+            const btns = document.getElementsByClassName("card-btn")
+            for (let i = 0; i < btns.length; i++) {
+                btns[i].addEventListener("click", agregarProducto)
+            }
+
+        })
+        .catch(err => console.error(err));
+
 }
 
 
-//CREO LOS OBJETOS "PRODUCTOS"
-const productos = [
-    new Product(1, "Guantes de Boxeo", 18500, 11),
-    new Product(2, "Tibiales", 21000, 8),
-    new Product(3, "Guantes de MMA", 14900, 6),
-    new Product(4, "Cabezal de Sparring", 12600, 4),
-    new Product(5, "Mouthguard", 4800, 18),
-    new Product(6, "Kimono JiuJitsu", 35000, 13),
-    new Product(7, "Bolso XL", 28000, 8),
-    new Product(8, "Hoodie", 15900, 15),
-    new Product(9, "Short MMA", 7500, 9)
-]
 
 
 //CREO UN ARRAY "CARRITO" VACIO
 const carrito = []
-
-
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (formulario.nombre.value === '' || formulario.apellido.value === '') {
-      advertencia.textContent = '¡Por favor complete todos los campos!';
-    } else {
-      formulario.submit();
-    }
-  });
 
 //FUNCION QUE GUARDA LOS DATOS DEL USUARIO EN EL STORAGE, O CORROBORA SI YA SE ENCUENTRA
 function userInStorage() {
@@ -58,14 +87,15 @@ function userInStorage() {
         e.preventDefault()
         if (formulario.nombre.value === '' || formulario.apellido.value === '') {
             advertencia.textContent = 'Complete todos los campos para ingresar.';
-          } else {
-        const usuario = {
-            nombre: inputNombre.value,
-            apellido: inputApellido.value,
+        } else {
+            const usuario = {
+                nombre: inputNombre.value,
+                apellido: inputApellido.value,
+            }
+            localStorage.setItem("Usuario", JSON.stringify(usuario)) //GUARDO EL OBJETO CREADO EN EL STORAGE
+            logeado(usuario); //UNA VEZ SETEADO EL OBJETO, EJECUTO LA FUNCION LOGEADO
         }
-        localStorage.setItem("Usuario", JSON.stringify(usuario)) //GUARDO EL OBJETO CREADO EN EL STORAGE
-        logeado(usuario); //UNA VEZ SETEADO EL OBJETO, EJECUTO LA FUNCION LOGEADO
-    }}
+    }
 
     const usuario = localStorage.getItem("Usuario")
     const infoUsuarioJSON = JSON.parse(usuario);
@@ -79,17 +109,10 @@ function userInStorage() {
 ELIMINA EL FORMULARIO, MODIFICA EL TEXTO DEL H2, AGREGA PRODUCTOS Y BOTON DE FINALIZAR AL DOM */
 function logeado(u) {
     formulario.remove();
+    advertencia.remove();
     h2.innerText = `Bienvenido a la tienda, ${u.nombre} ${u.apellido.toUpperCase()}!`
-    productos.forEach(prod => {
-        div.innerHTML += `<div class="card" style="width: 18rem;">
-        <div class="card-body">
-                <h5 class="card-title">${prod.nombre}</h5>
-                <p class="card-text">$${prod.precio}</p>
-                <button id=${prod.id} class="btn btn-warning" style="background-color:#DDDA56; color:585846">AGREGAR</button>  
-        </div>
-        </div>`
-    })
-    agregarProductos()
+    getJuegos()
+    //agregarProductos()
     deslogear()
     finalizarCompra()
 }
@@ -110,39 +133,45 @@ function deslogear() {
             confirmButtonColor: '#79CD7E',
             cancelButtonColor: '#C5BEBC',
             confirmButtonText: 'Salir'
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
                 localStorage.clear() //LIMPIO EL LOCALSTORAGE PARA ELIMINAR AL USUARIO, Y ASÍ TENER QUE COMPLETAR EL FORMULARIO DE INICIO NUEVAMENTE
                 location.reload(); //APLICO MÉTODO PARA ACTUALIZAR AUTOMÁTICAMENTE, Y LLEVAR AL USUARIO DENUEVO AL FORMULARIO DE INICIO
             }
-          })
+        })
     }
 }
 
 
 //FUNCION DE AGREGAR PRODUCTOS AL CARRITO, CONTROLANDO EL EVENTO ONCLICK DEL BOTON AGREGAR DE CADA PRODUCTO
-function agregarProductos() {
-    const botonesAgregar = Array.from(document.getElementsByClassName("btn-warning")); //BUSCO TODOS LOS BOTONES DEL DOM. CONVIERTO LA VARIABLE EN UN ARRAY PARA PODER APLICAR EL METODO FOREACH()
-    botonesAgregar.forEach(boton => {
-        boton.onclick = () => {
-            const producto = productos.find(p => p.id === parseInt(boton.id));
+const agregarProducto = (e) => {
+    const productoId = e.target.id
+    const product = productosFiltrados.filter(prod => prod.appId == productoId)[0] // TRAEMOS LA PRIMER POSICION PORQUE FILTER DEVUELVE EL OBJETO DENTRO DE UN ARRAY
 
-            const productoCarrito = { //CREO EL SIGUIENTE OBJETO A PARTIR DEL RESULTADO DE DICHA BÚSQUEDA, PARA USAR LUEGO EL ATRIBUTO CANTIDAD.
-                id: producto.id,
-                nombre: producto.nombre,
-                precio: producto.precio,
-                cantidad: 1,
-            }
+    const productoCarrito = { //CREO EL SIGUIENTE OBJETO A PARTIR DEL RESULTADO DE DICHA BÚSQUEDA, PARA USAR LUEGO EL ATRIBUTO CANTIDAD.
+        id: product.appId,
+        nombre: product.title,
+        precio: product.price,
+        cantidad: 1,
+    }
+    const isProductoEnCarrito = carrito.find(p => p.id === (productoCarrito.id)); // CREO UNA VARIABLE QUE BUSQUE SI EL PRODUCTO YA ESTÁ DENTRO DEL ARRAY CARRITO
 
-            const productoEnCarrito = carrito.find(p => p.id === (productoCarrito.id)); // CREO UNA VARIABLE QUE BUSQUE SI EL PRODUCTO YA ESTÁ DENTRO DEL ARRAY CARRITO
-            if (!productoEnCarrito) {
-                carrito.push(productoCarrito) //SI NO ESTÁ, SE LO PUSHEA AL ARRAY
-            } else {
-                productoEnCarrito.cantidad++ //SI ESTÁ, SE AUMENTA SU CANTIDAD EN +1
-            }
-            console.log(carrito);
-        }
-    })
+    if (!isProductoEnCarrito) {
+        carrito.push(productoCarrito) //SI NO ESTÁ, SE LO PUSHEA AL ARRAY
+        console.log(carrito)
+    } else {
+        isProductoEnCarrito.cantidad++ //SI ESTÁ, SE AUMENTA SU CANTIDAD EN +1
+        console.log(carrito)
+    }
+
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Agregado al carrito',
+        showConfirmButton: false,
+        timer: 700
+      })
+
 }
 
 
@@ -170,20 +199,21 @@ function finalizarCompra() {
         let totalCompra = 0
         carrito.forEach(productos => {
             totalCompra += productos.cantidad * productos.precio
+            totalFixed = totalCompra.toFixed(2);
             tbody.innerHTML += `
         <tr>
             <td>${productos.nombre}</td>
             <td>${productos.cantidad}</td>
-            <td>${productos.cantidad * productos.precio}</td>
+            <td>$${productos.cantidad * productos.precio}</td>
         </tr>`
         })
-        parrafoTotal.innerText = `El precio total de tu compra es:  $ ${totalCompra}`
+        parrafoTotal.innerText = `El precio total de tu compra es:  US$ ${totalFixed}`
         parrafoTotal.style.cssText = "color: #0B0F2E; font-size: 20px; font-weight: bold;"
         volverATienda()
     }
 }
 
-function volverATienda(){
+function volverATienda() {
     const usuario = localStorage.getItem("Usuario")
     const infoUsuarioJSON = JSON.parse(usuario) //DECLARO NUEVAMENTE ESTAS VARIABLES, YA QUE EXISTEN DENTRO DE LA FUNCION
     const volver = document.createElement("button") //CREO EL BOTON CORRESPONDIENTE
@@ -200,17 +230,17 @@ function volverATienda(){
             confirmButtonColor: '#79CD7E',
             cancelButtonColor: '#C5BEBC',
             confirmButtonText: 'Atrás'
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
-                logeado(infoUsuarioJSON) 
-                location.reload(); 
+                logeado(infoUsuarioJSON)
+                location.reload();
             }
-          })
+        })
     }
 }
 
 //CREO UNA FUNCION MAIN PARA MANTENER LA MODULARIZACIÓN Y ESTRUCTURACIÓN DEL CÓDIGO
-function main(){
+function main() {
     userInStorage() //LLAMA A LA FUNCION userInStorage(), LA CUAL ESTÁ RELACIONADA AL RESTO DE LAS FUNCIONES
 }
 
